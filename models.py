8d3677e0b27b5624d5c07b053256a16e44b78e58
Jacob Felknor <jacobfelknor073@gmail.com>
2020-10-15 12:01:02 -0600

    some setup

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
+
+Base = declarative_base()
+
+# this file is where we model Python classes after our db tables
+
+
+class Review(Base):
+    __tablename__ = "reviews"
+    reviewid = Column(Integer, primary_key=True)
+    title = Column(String)
+    artist = Column(String)
+    url = Column(String)
+    score = Column(Float)
+    best_new_music = Column(Integer)
+    author = Column(String)
+    author_type = Column(String)
+    pub_date = Column(String)
+    pub_weekday = Column(Integer)
+    pub_month = Column(Integer)
+    pub_year = Column(Integer)
