49a5ede9d8acc70513eee4d405cd4c61d85e0bf1
slundell123 <salu3240@colorado.edu>
2020-10-26 19:25:20 -0600

    Adding best new music average scores by year

--- reviewsOverTimeQueries.py
+++ reviewsOverTimeQueries.py
@@ -28,12 +28,37 @@ if __name__ == "__main__":
     # query get average score of ratings for each year
     averageScores = session.query(func.avg(Review.score), Review.pub_year).group_by(Review.pub_year).all()
 
-    fig, ax = plt.subplots()
+    # query get average score of ratings for each year
+    averageBestNewMusicScores = (
+        session.query(func.avg(Review.score), Review.pub_year)
+        .filter(Review.best_new_music == 1)
+        .group_by(Review.pub_year)
+        .all()
+    )
+
+    # Graphing score averages
+    fig, (ax1, ax2) = plt.subplots(1, 2)
 
     averageScores = sorted(averageScores, key=lambda x: (-x[1], x[0]))
-    print(averageScores)
     scores = [i[0] for i in averageScores]
     years = [i[1] for i in averageScores]
-    ax.plot(years, scores)
-    ax.set_xticks(years)
+    ax1.plot(years, scores)
+    ax1.set_xticks(years)
+    ax1.set_title("Music Score Average by Year")
+    ax1.set_ylabel("Average Score")
+    ax1.set_xlabel("Year")
+
+    averageBestNewMusicScores = sorted(averageBestNewMusicScores, key=lambda x: (-x[1], x[0]))
+    bestMusicScores = [i[0] for i in averageBestNewMusicScores]
+    bestMusicYears = [i[1] for i in averageBestNewMusicScores]
+    ax2.plot(bestMusicYears, bestMusicScores)
+    ax2.set_xticks(bestMusicYears)
+    ax2.set_title("Best New Music Score Average by Year")
+    ax2.set_ylabel("Average Score")
+    ax2.set_xlabel("Year")
+
+    fig.set_size_inches(15, 5, forward=True)
+    plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
+    plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
+
     plt.show()
